bcb602b2e168eefb2e8efb0680c9c82356e604e9
rakesh-godamwale <rakesh@godamwale.com>
2022-08-31 02:44:45 +0530

    command line deployment step file added

---
+++ deployment-steps.js
@@ -0,0 +1,6 @@
+// docker build -t truck-app .
+// docker run -p 8888:3000 truck-app // for testing whether container is working or not
+//1. tag image to gcr.io
+// $ docker tag truck-app gcr.io/transfort/truck-app
+// 2. push the container to gcp container registry
+// $ gcloud docker -- push gcr.io/transfort/truck-app
