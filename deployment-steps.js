5778aee254bd5acc62c39a807aa7413311012105
rakesh-godamwale <rakesh@godamwale.com>
2022-10-25 21:18:01 +0530

    gcp updated

--- deployment-steps.js
+++ deployment-steps.js
@@ -1,6 +1,6 @@
 // docker build -t truck-app .
 // docker run -p 8888:3000 truck-app // for testing whether container is working or not
 //1. tag image to gcr.io
-// $ docker tag truck-app gcr.io/transfort/truck-app
+// $ docker tag truck-app gcr.io/transfort-366614/truck-app
 // 2. push the container to gcp container registry
-// $ gcloud docker -- push gcr.io/transfort/truck-app
+// $ gcloud docker -- push gcr.io/transfort-366614/truck-app
